9a5bacd2e5794815d1f9d074e4107e27ac0057f9
manu <tresr3@msn.com>
2024-11-27 15:13:48 +0100

    create posts data fake in the dataMocks directory

---
+++ src/dataMocks/posts.ts
@@ -0,0 +1,20 @@
+import { Post } from "../models/post/post";
+
+export const POSTS_DATASET = [
+    {
+        name: "Pedro",
+        message: "Felicidades a todos"
+    },
+    {
+        name: "Maria",
+        message: "Tengo ganas de ver a mis hijos pablo y rosa"
+    },
+    {
+        name: "Jose",
+        message: "Hermanos es hora de perdonar"
+    },
+    {
+        name: "Lala",
+        message: "Porque solo yo no tengo pareja joder"
+    }
+] as Post[];
