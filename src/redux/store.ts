3ee1bd2746b092af61b29106673b6074008dab3e
manu <tresr3@msn.com>
2024-11-27 15:19:59 +0100

    fix name from interface and var name from (Store, initialStore) to (State, initialState)

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,11 +2,11 @@ import { configureStore  } from "@reduxjs/toolkit";
 import postListReducer, { PostListState } from "./reducers/posts/postListSlice";
 import { POSTS_DATASET } from "../dataMocks/posts";
 
-interface Store {
+interface State {
     postList: PostListState;
 }
 
-const initialStore: Store = {
+const initialState: State = {
     postList:  {
           posts: POSTS_DATASET
     },
@@ -16,7 +16,7 @@ export const store = configureStore({
     reducer: {
         postList: postListReducer,  
     },
-    preloadedState: initialStore
+    preloadedState: initialState
 });
 
 export type RootState = ReturnType<typeof store.getState>;
