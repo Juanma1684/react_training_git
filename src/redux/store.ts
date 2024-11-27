b535e00ec42e0961e97895fcd90b279183c74d9e
manu <tresr3@msn.com>
2024-11-27 15:15:02 +0100

    fix initial store state to get posts correcly

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,22 @@
 import { configureStore  } from "@reduxjs/toolkit";
-import postListReducer from "./reducers/posts/postListSlice";
+import postListReducer, { PostListState } from "./reducers/posts/postListSlice";
+import { POSTS_DATASET } from "../dataMocks/posts";
+
+interface Store {
+    postList: PostListState;
+}
+
+const initialStore: Store = {
+    postList:  {
+          posts: POSTS_DATASET
+    },
+}
 
 export const store = configureStore({
     reducer: {
         postList: postListReducer,  
     },
+    preloadedState: initialStore
 });
 
 export type RootState = ReturnType<typeof store.getState>;
